f9797bd82ab872587f87ac2a120739e7f59bdce1
ANDRIAMANANTENA Mamitiana Rianala <mamitianarianala404@gmail.com>
2025-03-21 18:53:50 +0300

    ResetService

---
+++ DaybydayCRM-master/app/Services/Reset/ResetService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services\Reset;
+
+use Illuminate\Support\Facades\DB;
+
+class ResetService
+{
+    public static function resetDatabase()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+    
+        $tablesToReset = [
+            'mails',
+            'comments',
+            'absences',
+            'leads',
+            'projects',
+            'tasks',
+            'contacts',
+            'industries',
+            'appointments',
+            'clients',
+            'offers',
+            'invoices',
+            'invoice_lines',
+            'payments'
+        ];
+    
+        foreach (DB::select('SHOW TABLES') as $table) {
+            $tableName = array_values((array) $table)[0];
+    
+            if (in_array($tableName, $tablesToReset)) {
+                DB::table($tableName)->truncate();
+            }
+        }
+    
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+    }
+    
+}
